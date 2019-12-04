2dbca4e3cd99e7b6b004fa2ddc80755fba14b1c3
Laurence Smith <laurence.smith@airelogic.com>
2019-12-04 11:59:55 +0000

    APM-78 fix syntax issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $CIRCLE_BRANCH = "master" ] then
+if [ $CIRCLE_BRANCH = "master" ]; then
     curl --fail -H "Content-Type: application/x-www-form-urlencoded;charset=utf-8" -H "Accept: application/json;charset=utf-8" -H "Authorization: Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0" -X POST https://login.apigee.com/oauth/token -d 'username='$APIGEE_USER'&password='$APIGEE_PASS'&grant_type=password' | jq -r .access_token > /tmp/access_token
     echo 'export APIGEE_ACCESS_TOKEN=$(cat /tmp/access_token)' >> $BASH_ENV
     curl --fail -X PUT 'https://apigee.com/dapi/api/organizations/emea-demo8/specs/doc/'$APIGEE_SPEC_ID'/content' -H 'Authorization: Bearer '$APIGEE_ACCESS_TOKEN'' -H 'Content-Type: text/plain' --data '@dist/patient-information-api.json'
