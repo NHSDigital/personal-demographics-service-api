e9886c3e5695abddb59fd7bc6da3fa3855c5aad1
Alex Hawdon <alex.hawdon@airelogic.com>
2019-12-06 14:16:18 +0000

    Stub API server
    Initial NodeJS (hapi) implementation.

---
+++ stubserver/Readme.md
@@ -0,0 +1,47 @@
+# Stub API Server
+
+Stub API Server built using [hapi](https://hapi.dev/) framework deployable as a [Apigee Hosted Target](https://docs.apigee.com/api-platform/hosted-targets/hosted-targets-overview).
+
+Intended for "sandbox" functionality, and is the target endpoint for the hosted docs' *Try it now* functionality.
+
+## Developing
+
+```
+npm install
+npm run serve
+```
+
+ * Use the examples from the OAS (`components/examples/`) sym-linking them into the app.
+
+## Deployment
+
+```
+npm run deploy
+```
+
+Will collect all the assets in the `dist/` folder and create a new **undeployed** revision of the *Patient-Information* API Proxy.
+
+Requires a username and password.
+
+Uses [apigeetool](https://www.npmjs.com/package/apigeetool).
+
+## Endpoints
+
+- [ ] GET    `/Patient`
+- [x] GET    `/Patient/{nhs_number}`
+- [ ] PATCH  `/Patient/{nhs_number}`
+- [ ] GET    `/Patient/{nhs_number}/RelatedPerson`
+- [ ] POST   `/Patient/{nhs_number}/RelatedPerson`
+- [ ] GET    `/Patient/{nhs_number}/RelatedPerson/{id}`
+- [ ] PUT    `/Patient/{nhs_number}/RelatedPerson/{id}`
+- [ ] DELETE `/Patient/{nhs_number}/RelatedPerson/{id}`
+- [ ] GET    `/Patient/{nhs_number}/ReasonableAdjustment`
+- [ ] POST   `/Patient/{nhs_number}/ReasonableAdjustment`
+- [ ] GET    `/Patient/{nhs_number}/ReasonableAdjustment/{id}`
+- [ ] PUT    `/Patient/{nhs_number}/ReasonableAdjustment/{id}`
+- [ ] DELETE `/Patient/{nhs_number}/ReasonableAdjustment/{id}`
+- [ ] GET    `/Patient/{nhs_number}/Address`
+- [ ] POST   `/Patient/{nhs_number}/Address`
+- [ ] GET    `/Patient/{nhs_number}/Address/{id}`
+- [ ] PUT    `/Patient/{nhs_number}/Address/{id}`
+- [ ] DELETE `/Patient/{nhs_number}/Address/{id}`
---
+++ stubserver/app.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const Hapi = require('@hapi/hapi')
+const Path = require('path')
+const Inert = require('inert')
+const Joi = require('@hapi/joi')
+const Boom = require('boom')
+const fs = require('fs')
+
+
+const EXAMPLE_PATIENT = JSON.parse(fs.readFileSync('mocks/Patient.json'))
+
+const nhsNumberSchema = Joi.string().pattern(/^\d{10}$/)
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 9000,
+        host: '0.0.0.0',
+        routes: {
+            cors: true,  // Won't run as Apigee hosted target without this
+            files: {
+                relativeTo: Path.join(__dirname, 'mocks')
+            }
+        }
+    })
+
+    await server.register(Inert)
+
+    server.route({
+        method: 'GET',
+        path: '/Patient/{nhsNumber}',
+        handler: (request, h) => {
+            // Validate NHS number is valid format
+            // Ideally should be done using options.validate object (https://hapi.dev/tutorials/validation/)
+            // But don't know how to customise the returned JSON when done this way
+            const validationResult = nhsNumberSchema.validate(request.params.nhsNumber)
+            if (validationResult.error) {
+                const error = Boom.badRequest()
+                error.output.payload = {
+                    code: "invalid_nhs_number",
+                    message: "NHS number is not in the correct format, or is not a real NHS number"
+                }
+                throw error
+            }
+
+            // Validate NHS number is for our test patient
+            if (request.params.nhsNumber != EXAMPLE_PATIENT.nhs_number) {
+                const error = Boom.notFound()
+                error.output.payload = {
+                    code: "not_found",
+                    message: `Patient with NHS number ${request.params.nhsNumber} could not be found`
+                }
+                throw error
+            }
+
+            return h.response(EXAMPLE_PATIENT)
+                .etag(EXAMPLE_PATIENT._version, { weak: true })
+        }
+    })
+
+    await server.start()
+    console.log('Server running on %s', server.info.uri)
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
+init()
