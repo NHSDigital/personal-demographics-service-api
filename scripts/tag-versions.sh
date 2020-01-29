fae1d0db160eb89fc276f41f8bb56741cbaa5026
Laurence Smith <laurence.smith@airelogic.com>
2020-01-29 12:30:27 +0000

    APM-84 add branch condition for tagging

--- scripts/tag-versions.sh
+++ scripts/tag-versions.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 
 VERSION=$(poetry run python scripts/calculate_version.py)
 
-
-git config credential.helper 'cache --timeout 120'
-git config user.email "<email>"
-git config user.name "Marvin (Automated)"
-git tag "$VERSION"
-git push -q https://${GITHUB_ACCESS_TOKEN}@github.com/NHSDigital/personal-demographics-service-api "$VERSION" > /dev/null 2>&1
+if [ $CIRCLE_BRANCH = 'apm-84-versioning' ]; then
+    git config credential.helper 'cache --timeout 120'
+    git config user.email "<email>"
+    git config user.name "Marvin (Automated)"
+    git tag "$VERSION"
+    git push -q https://${GITHUB_ACCESS_TOKEN}@github.com/NHSDigital/personal-demographics-service-api "$VERSION" > /dev/null 2>&1
+fi
