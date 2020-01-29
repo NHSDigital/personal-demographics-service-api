f1a0a72e2324837ffe88719dd8b338bc0a460e9c
Laurence Smith <laurence.smith@airelogic.com>
2020-01-29 12:12:42 +0000

    APM-84 Push with personal access token

--- scripts/tag-versions.sh
+++ scripts/tag-versions.sh
@@ -3,5 +3,10 @@
 set -euo pipefail
 
 VERSION=$(poetry run python scripts/calculate_version.py)
+
+
+git config credential.helper 'cache --timeout 120'
+git config user.email "<email>"
+git config user.name "Marvin (Automated)"
 git tag "$VERSION"
-git push origin "$VERSION"
+git push -q https://${GITHUB_ACCESS_TOKEN}@github.com/NHSDigital/personal-demographics-service-api "$VERSION" > /dev/null 2>&1
