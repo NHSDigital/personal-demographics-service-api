0c8d4aa2e9f228f9605bc4face9ce6ea5c025981
Henry Iwanejko <henry.iwanejko1@nhs.net>
2020-02-10 14:38:03 +0000

    APM-247 Add tag-version to pipeline

--- scripts/tag-versions.sh
+++ scripts/tag-versions.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 VERSION=$(poetry run python scripts/calculate_version.py)
 
-if [ $CIRCLE_BRANCH = 'master' ]; then
-    git config credential.helper 'cache --timeout 120'
-    git config user.email "<email>"
-    git config user.name "Marvin (Automated)"
-    git tag "$VERSION"
-    git push -q https://${GITHUB_ACCESS_TOKEN}@github.com/NHSDigital/personal-demographics-service-api "$VERSION" > /dev/null 2>&1
-fi
+# if [ $CIRCLE_BRANCH = 'master' ]; then
+#     git config credential.helper 'cache --timeout 120'
+#     git config user.email "<email>"
+#     git config user.name "Marvin (Automated)"
+#     git tag "$VERSION"
+#     git push -q https://${GITHUB_ACCESS_TOKEN}@github.com/NHSDigital/personal-demographics-service-api "$VERSION" > /dev/null 2>&1
+# fi
