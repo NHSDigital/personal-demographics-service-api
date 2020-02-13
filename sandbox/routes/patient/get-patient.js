5a696924dc0b00782a20c5fecfaad499af7f5710
Henry Iwanejko <henry.iwanejko1@nhs.net>
2020-02-13 16:32:49 +0000

    APM-270 Add Example Patient

--- sandbox/routes/patient/get-patient.js
+++ sandbox/routes/patient/get-patient.js
@@ -4,19 +4,21 @@ const fhirHelper = require('../../helpers/fhir-helper')
 const nhsNumberHelper = require('../../helpers/nhs-number-helper')
 const dateValidator = require('../../validators/date-validator')
 
-const EXAMPLE_PATIENT = JSON.parse(fs.readFileSync('mocks/Patient-Jane-Smith.json'))
+const EXAMPLE_PATIENT_SMITH = JSON.parse(fs.readFileSync('mocks/Patient-Jane-Smith.json'))
+const EXAMPLE_PATIENT_SMYTHE = JSON.parse(fs.readFileSync('mocks/Patient-Jayne-Smyth.json'))
+
 
 module.exports = [
     /* Patient Search
         Behaviour Implemented:
-            * Provide ?9000000009: recieve example patient record as search result
+         * Provide ?9000000009: recieve example patient record as search result
     */
     {
         method: 'GET',
         path: '/Patient/{nhsNumber}',
         handler: (request, h) => {
             nhsNumberHelper.checkNhsNumber(request)
-            return fhirHelper.createFhirResponse(h, EXAMPLE_PATIENT)
+            return fhirHelper.createFhirResponse(h, EXAMPLE_PATIENT_SMITH)
         }
     },
 
@@ -31,12 +33,16 @@ module.exports = [
         method: 'GET',
         path: '/Patient',
         handler: (request) => {
-
+            
             // TODO: Spilt down methods into seperate files
 
             // TODO: This can be provided to a PatientSearcher to use to implement a more
             // 'proper' search
             const searchMap = {
+                "_exact-match": true,
+                "_history": true,
+                "_fuzzy-match": true,
+                "_max-results": true,
                 family: '$.name[?(@.use="usual")].family', // Usual family name
                 given: true,
                 gender: true,
@@ -81,29 +87,77 @@ module.exports = [
                 entry: []
             }
 
-            // Perform a 'simple search'
-            const simpleSearchParams = {
-                family: "Smith",
-                given: "Jane",
-                gender: "female",
-                birthdate: "2010-10-22",
-            }
-            let simpleMatch = true
-            for (let p of Object.keys(simpleSearchParams)) {
-                if (!request.query[p] || request.query[p].toLowerCase() !== simpleSearchParams[p].toLowerCase()) {
-                    simpleMatch = false
-                    break
-                }
-            }
-            // If so, try it
-            if (simpleMatch) {
+            // Build a response with one patient
+            let examplePatientResponse = function() {
                 response.total = 1
                 response.entry.push({
                     search: {
                         score: 1.0
                     },
-                    resource: EXAMPLE_PATIENT,
+                    resource: EXAMPLE_PATIENT_SMITH,
                 })
+                return response
+            }
+
+            // Build example response with 2 patients
+            let exampleResponseWithTwoPatients = function () {
+                response.total = 2
+                response.entry.push({
+                    search: {
+                        score: 0.8343
+                    },
+                        resource: [EXAMPLE_PATIENT_SMITH, EXAMPLE_PATIENT_SMYTHE]
+                })
+                return response
+            }
+
+            // Can be moved elsewhere
+            let containsSearchParameters = function(searchParameters) {
+                for (let p of Object.keys(searchParameters)) {
+                    if (!request.query[p] || request.query[p].toLowerCase() !== searchParameters[p].toLowerCase()) {
+                        return false
+                    }
+                }
+                return true
+            }
+
+
+            // Check if wildcard provided
+            const  wildcardSearchParams = {
+                family: "Sm*",
+                gender: "female",
+                birthdate: "2010-10-22"
+            }
+            let wildcardMatch = containsSearchParameters(wildcardSearchParams)
+
+            // Perform a search with max result set using the wildcard params and the max-result parameter
+            if (wildcardMatch && request.query["_max-results"]) {
+                if (isNaN(request.query["_max-results"]) || request.query["_max-results"] < 1) {
+                    // Happy path only (For now)
+                    throw Boom.badRequest("TBC", {
+                        operationOutcomeCode: "TBC", apiErrorCode: "TBC"
+                    })
+                } else if (request.query["_max-results"] == 1) {
+                    return examplePatientResponse()
+                } else {
+                    return exampleResponseWithTwoPatients()
+                }
+            // Perform a advanced search as wildcard provided and max-result parameter not set
+            } else if (wildcardMatch) {
+                return exampleResponseWithTwoPatients()
+            }
+
+
+            // Perform a 'simple search'
+            const simpleSearchParams = {
+                family: "Smith",
+                gender: "female",
+                birthdate: "2010-10-22",
+            }
+            let simpleMatch = containsSearchParameters(simpleSearchParams)
+            // If so, try it
+            if (simpleMatch) {
+                return examplePatientResponse()
             }
 
             return response
