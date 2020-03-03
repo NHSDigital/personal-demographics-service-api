5edabc6383e8cc611f531ba8021bf22e64865e72
Henry Iwanejko <henry.iwanejko1@nhs.net>
2020-03-03 14:52:00 +0000

    APM-384-Changes-from-review

--- scripts/deploy_proxy.sh
+++ scripts/deploy_proxy.sh
@@ -12,5 +12,5 @@ sed "s/PROXY_BASE_PATH/$APIGEE_BASE_PATH/g" dist/apiproxy/proxies/default.xml > 
 cp -f dist/apiproxy/proxies/default_temp.xml dist/apiproxy/proxies/default.xml
 rm -rf dist/apiproxy/proxies/default_temp.xml
 mkdir -p dist/apiproxy/resources/hosted
-rsync -av --copy-links --exclude="node_modules" sandbox/ dist/apiproxy/resources/hosted
+rsync -av --copy-links --exclude="node_modules" --filter=':- .gitignore' sandbox/ dist/apiproxy/resources/hosted
 node_modules/.bin/apigeetool deployproxy --environments "$APIGEE_ENVIRONMENTS" --api "$APIGEE_APIPROXY" --directory dist/ --verbose
