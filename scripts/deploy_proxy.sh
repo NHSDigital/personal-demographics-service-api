138d7761f05bc7de221e74f60fbd4fc8833f5966
Henry Iwanejko <henry.iwanejko1@nhs.net>
2020-03-03 14:18:04 +0000

    APM-384 Update deploy script to run on macOS

--- scripts/deploy_proxy.sh
+++ scripts/deploy_proxy.sh
@@ -7,8 +7,10 @@ set -o nounset errexit pipefail
 
 mkdir -p dist
 rm -rf dist/apiproxy
-cp -R apiproxy/ dist/
-sed -i "s/PROXY_BASE_PATH/$APIGEE_BASE_PATH/g" dist/apiproxy/proxies/default.xml
+cp -Rv apiproxy dist/
+sed "s/PROXY_BASE_PATH/$APIGEE_BASE_PATH/g" dist/apiproxy/proxies/default.xml > dist/apiproxy/proxies/default_temp.xml
+cp -f dist/apiproxy/proxies/default_temp.xml dist/apiproxy/proxies/default.xml
+rm -rf dist/apiproxy/proxies/default_temp.xml
 mkdir -p dist/apiproxy/resources/hosted
 rsync -av --copy-links --exclude="node_modules" sandbox/ dist/apiproxy/resources/hosted
 node_modules/.bin/apigeetool deployproxy --environments "$APIGEE_ENVIRONMENTS" --api "$APIGEE_APIPROXY" --directory dist/ --verbose
