b90ea27cf2e5a0d7bb16e4379ebf1e2778e0f7d7
Laurence Smith <laurence.smith@airelogic.com>
2020-04-17 14:56:31 +0100

    APM-501 Refactor test runner

--- tests/e2e/test-runner.js
+++ tests/e2e/test-runner.js
@@ -1,19 +1,27 @@
+doc = `
+API Management Postman Test Runner
+
+Usage:
+  test-runner.js <username> <password> <token_app_url> <environment_url> <collection_path> <environment_file_path>
+  test-runner.js -h | --help
+
+  -h --help  Show this text.
+`;
+
+
+const fs = require('fs');
+const path = require('path');
+const docopt = require('docopt').docopt;
 const newman = require('newman');
 const puppeteer = require('puppeteer');
 
-const username = '910000000001';
-const password = 'Password1';
-var access_token = '';
-var jwt_token = '';
 
-
-function nhsIdLogin(_callback) {
+function nhsIdLogin(username, password, login_url, callback) {
     (async () => {
-        let herokuapp_url = process.argv[3];
-        console.log("Oauth journey on " + herokuapp_url);
+        console.log("Oauth journey on " + login_url);
         const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
-        await page.goto(herokuapp_url, { waitUntil: 'networkidle2' });
+        await page.goto(login_url, { waitUntil: 'networkidle2' });
         await page.click("#start");
         await page.waitForSelector('#idToken1');
         await page.type('#idToken1', username);
@@ -21,68 +29,83 @@ function nhsIdLogin(_callback) {
         await page.click('#loginButton_0');
         await page.waitForNavigation();
 
-        let tokenJson = await page.$eval('body > div > div > pre', e => e.innerText);
-        setTokens(tokenJson);
+        let credentialsJSON = await page.$eval('body > div > div > pre', e => e.innerText);
+        console.log(credentialsJSON.replace(/'/g, '"'));
+        let credentials = JSON.parse(credentialsJSON.replace(/'/g, '"'));
         await browser.close();
-        _callback();
+        callback(credentials);
     })();
 }
 
-function setTokens(json) {
-    let data = JSON.parse(json);
-    access_token = json.access_token;
-    jwt_token = json.id_token;
-}
+function collectionRunner(url, collection_path, environment_path) {
+    const collection = JSON.parse(fs.readFileSync(
+        path.resolve(collection_path)
+    ));
+    const environment = JSON.parse(fs.readFileSync(
+        path.resolve(environment_path)
+    ));
 
-function runPostmanCollection() {
-    newman.run({
-        collection: require(process.argv[2]),
-        reporters: ['cli', 'junit'],
-        reporter: {
-            junit: {
-                export: './test-report.xml'
-            }
-        },
-        globals: {
-            "id": "5bfde907-2a1e-8c5a-2246-4aff74b74236",
-            "name": "global-params",
-            "values": [
-                {
-                    "key": "token",
-                    "value": access_token,
-                    "type": "text",
-                    "enabled": true
-                },
-                {
-                    "key": "jwt",
-                    "value": jwt_token,
-                    "type": "text",
-                    "enabled": true
-                },
-                {
-                    "key": "environment",
-                    "value": process.argv[4],
-                    "type": "text",
-                    "enabled": true
+    return (credentials) => {
+        newman.run({
+            collection: collection,
+            reporters: ['cli', 'junit'],
+            reporter: {
+                junit: {
+                    export: './test-report.xml'
                 }
-            ],
-            "timestamp": 1404119927461,
-            "_postman_variable_scope": "globals",
-            "_postman_exported_at": "2020-04-03T14:31:26.200Z",
-            "_postman_exported_using": "Postman/4.8.0"
-        },
-        environment: require(process.argv[5])
-    }, function (err) {
-        if (err) { throw err; }
-        console.log('collection run complete!');
-    }).on('start', function (err, args) {
-        console.log('Running against ' + process.argv[4]);
-        console.log('Using environment file ' + process.argv[5]);
-    });
+            },
+            globals: {
+                "id": "5bfde907-2a1e-8c5a-2246-4aff74b74236",
+                "name": "global-params",
+                "values": [
+                    {
+                        "key": "token",
+                        "value": credentials.access_token,
+                        "type": "text",
+                        "enabled": true
+                    },
+                    {
+                        "key": "jwt",
+                        "value": credentials._token,
+                        "type": "text",
+                        "enabled": true
+                    },
+                    {
+                        "key": "environment",
+                        "value": url,
+                        "type": "text",
+                        "enabled": true
+                    }
+                ],
+                "timestamp": 1404119927461,
+                "_postman_variable_scope": "globals",
+                "_postman_exported_at": "2020-04-03T14:31:26.200Z",
+                "_postman_exported_using": "Postman/4.8.0"
+            },
+            environment: environment
+        }, function (err) {
+            if (err) { throw err; }
+            console.log('collection run complete!');
+        }).on('start', function (err, args) {
+            console.log('Running against ' + url);
+            console.log('Using collection file ' + collection_path);
+            console.log('Using environment file ' + environment_path);
+        });
+    };
 }
 
-function run() {
-    nhsIdLogin(runPostmanCollection);
+function main(args) {
+    nhsIdLogin(
+        args['<username>'],
+        args['<password>'],
+        args['<token_app_url>'],
+        collectionRunner(
+            args['<environment_url>'],
+            args['<collection_path>'],
+            args['<environment_file_path>']
+        )
+    );
 }
 
-run();
+args = docopt(doc);
+main(args);
