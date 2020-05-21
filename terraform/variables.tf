875343a46278e9fa10daa2508725a938d655ffaf
jalal <jalalhosseiny@gmail.com>
2020-05-21 14:10:42 +0100

    AMB-52 fix wrong condition on ping endpoint. Remove terraform variables default values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,23 +25,19 @@ variable "force_sandbox" {
   default = false
 }
 
-// FIXME remove default values
 variable "status_cake_username" {
     type = string
     description = "Statuscake username for monitoring and alerting"
-    default = ""
 }
 
 variable "status_cake_api_key" {
     type = string
     description = "Statuscake apikey for monitoring and alerting"
-    default = ""
 }
 
 variable "status_cake_contact_group" {
     type = string
     description = "Statuscake Contact Group for monitoring and alerting"
-    default = ""
 }
 
 // FIXME: remove it
