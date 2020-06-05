10c351509237af3a30a52e0c61da81f46909815d
jalal <jalalhosseiny@gmail.com>
2020-06-05 15:17:48 +0100

    AMB-68 fix dependency path

--- sandbox/routes/index.js
+++ sandbox/routes/index.js
@@ -1,5 +1,5 @@
 const patientRoutes = require('./patient')
-const statusRoutes = require('get-status')
+const statusRoutes = require('./get-status')
 
 const routes = [].concat(patientRoutes, statusRoutes)
 
