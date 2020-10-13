411ae6d70e9b477ce17e2196806b44ac3fabc1e0
Paul Walkington <paul.walkington@infinityworks.com>
2020-10-13 13:56:51 +0100

    APMSPII-242 - added policy for creating healthcheck response

---
+++ proxies/live/apiproxy/resources/jsc/SetStatusResponse.js
@@ -0,0 +1,38 @@
+
+var apiproxy_revision = context.getVariable('apiproxy.revision');
+var spine_response_code = context.getVariable('response.status.code');
+
+var spine_status = "fail";
+
+if(spine_response_code/ 100 == 2){
+    spine_status = "pass";
+}
+
+var spine_service = {
+"spine:status" : [
+    {
+    "status": spine_status, 
+    "timeout" : "false",
+    "responseCode" : spine_response_code,
+   }]
+}
+
+var apigee_status = "pass"
+
+if(spine_status != "pass"){
+    apigee_status = "fail"
+}
+
+
+
+var response = { 
+"status" : apigee_status,
+"version" : "personal-demographics-pr-394" ,
+"revision" : apiproxy_revision, 
+"releaseId" : "10111", 
+"commitId": "911d8dee679a7cd5ab0d337e10fcaa9f76ff75f8",
+"checks" : spine_service
+};
+
+
+context.proxyResponse.content = JSON.stringify(response);
