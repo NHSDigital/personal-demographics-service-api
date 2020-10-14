b09ee7af3c68c7f88b3a57bc9e5d803f8b95e680
Paul Walkington <paul.walkington@infinityworks.com>
2020-10-14 13:16:24 +0100

    APMSPII-242 - renamed targetBasePath to targetPath

--- proxies/live/apiproxy/resources/jsc/SetTargetPath.js
+++ proxies/live/apiproxy/resources/jsc/SetTargetPath.js
@@ -1,9 +1,9 @@
 var proxy_pathsuffix = context.getVariable('proxy.pathsuffix');
-var targetBasePath = "";
+var targetPath = "";
 
 if (proxy_pathsuffix == "/_status") {
-    targetBasePath = "/healthcheck";
+    targetPath = "/healthcheck";
     context.setVariable('target.copy.pathsuffix', false);
 }
 
-context.setVariable("targetBasePath", targetBasePath);
+context.setVariable("targetPath", targetPath);
