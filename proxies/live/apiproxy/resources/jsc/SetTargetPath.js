f5ba89e0b54250f43e4d7e3a0be6b604709b1386
Paul Walkington <paul.walkington@infinityworks.com>
2020-10-15 11:06:56 +0100

    APMSPII-242 - remove setting of url path using policy

--- proxies/live/apiproxy/resources/jsc/SetTargetPath.js
+++
@@ -1,9 +0,0 @@
-var proxy_pathsuffix = context.getVariable('proxy.pathsuffix');
-var targetPath = "";
-
-if (proxy_pathsuffix == "/_status") {
-    targetPath = "/healthcheck";
-    context.setVariable('target.copy.pathsuffix', false);
-}
-
-context.setVariable("targetPath", targetPath);
