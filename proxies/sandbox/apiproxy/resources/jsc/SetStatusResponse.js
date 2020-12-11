b8807d99ba432f51e8e8a72180975f92c90a5540
Alex Hawdon <alex.hawdon@airelogic.com>
2020-12-11 14:13:06 +0000

    formatting

--- proxies/sandbox/apiproxy/resources/jsc/SetStatusResponse.js
+++ proxies/sandbox/apiproxy/resources/jsc/SetStatusResponse.js
@@ -8,44 +8,43 @@ var sandbox_request_has_failed = context.getVariable("servicecallout.ServiceCall
 
 var sandbox_status = "fail";
 
-if(sandbox_response_code/ 100 == 2){
-    sandbox_status = "pass";
+if (sandbox_response_code / 100 == 2) {
+  sandbox_status = "pass";
 }
 
 timeout = "false";
 
-if(sandbox_response_code === null && sandbox_request_has_failed){
-    timeout = "true";
+if (sandbox_response_code === null && sandbox_request_has_failed) {
+  timeout = "true";
 }
 
 
 
 var sandbox_service = {
-"sandbox:status" : [
-    {
-    "status": sandbox_status, 
-    "timeout" : timeout,
-    "responseCode" : sandbox_response_code,
+  "sandbox:status": [{
+    "status": sandbox_status,
+    "timeout": timeout,
+    "responseCode": sandbox_response_code,
     "outcome": sanbox_response,
-    "links" : {"self": sandbox_request_url}
-   }]
+    "links": { "self": sandbox_request_url }
+  }]
 };
 
 var apigee_status = "pass";
 
-if(sandbox_status != "pass"){
-    apigee_status = "fail";
+if (sandbox_status != "pass") {
+  apigee_status = "fail";
 }
 
 
 
-var response = {  
-"status" : apigee_status,
-"version" : "{{ DEPLOYED_VERSION }}" ,
-"revision" : apiproxy_revision, 
-"releaseId" : "{{ RELEASE_RELEASEID }}", 
-"commitId": "{{ SOURCE_COMMIT_ID }}",
-"checks" : sandbox_service
+var response = {
+  "status": apigee_status,
+  "version": "{{ DEPLOYED_VERSION }}",
+  "revision": apiproxy_revision,
+  "releaseId": "{{ RELEASE_RELEASEID }}",
+  "commitId": "{{ SOURCE_COMMIT_ID }}",
+  "checks": sandbox_service
 };
 
 context.setVariable("status.response", JSON.stringify(response));
