2bac9df2a77559aeadab097a892f14c289fbafa1
Alex Hawdon <alex.hawdon@airelogic.com>
2021-01-22 18:50:30 +0000

    Remove old sandbox tests, run new tests on release
    Also new make command and instructions to run tests locally

--- tests/sandbox/configuration/config.py
+++ tests/sandbox/configuration/config.py
@@ -1,9 +1,12 @@
 from .environment import ENV
 
 
-# Apigee Details
+# Sandbox Details
 ENVIRONMENT = ENV["environment"]
-BASE_URL = f"https://{ENVIRONMENT}.api.service.nhs.uk"
 
-# PDS
-PDS_BASE_PATH = ENV["pds_base_path"]
+if ENVIRONMENT.lower() == "local":
+    SANDBOX_BASE_URL = "http://0.0.0.0:9000"
+else:
+    PDS_BASE_PATH = ENV["pds_base_path"]
+    SANDBOX_BASE_URL = f"https://{ENVIRONMENT}.api.service.nhs.uk/{PDS_BASE_PATH}"
+
