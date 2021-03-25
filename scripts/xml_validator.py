87034c606df5eb322816c7340e2a8fdc33122a44
paruliansaragi <parulian.saragi1@nhs.net>
2021-03-25 11:29:07 +0000

    fix linting xml validator

--- scripts/xml_validator.py
+++ scripts/xml_validator.py
@@ -14,7 +14,7 @@ for file in glob2.glob('./proxies/**/*.xml'):
     try:
         parsefile(file)
     except Exception as e:
-        print("\x1b[0;30;41m" + f"{file} is NOT well-formed! {e}" + "\x1b[0m" )
+        print("\x1b[0;30;41m" + f"{file} is NOT well-formed! {e}" + "\x1b[0m")
         sys.exit(1)
 
 sys.exit(0)
