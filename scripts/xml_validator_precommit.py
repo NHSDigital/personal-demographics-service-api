f579d8036226392e26edb4fa2e4f8c5954e5679b
paruliansaragi <parulian.saragi1@nhs.net>
2021-03-25 10:18:52 +0000

    Add sys exit to precommit

--- scripts/xml_validator_precommit.py
+++ scripts/xml_validator_precommit.py
@@ -1,7 +1,7 @@
 import glob2
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
-
+import sys
 
 def parsefile(file):
     parser = make_parser()
@@ -14,3 +14,4 @@ for file in glob2.glob('./proxies/**/*.xml'):
         parsefile(file)
     except Exception as e:
         print("%s is NOT well-formed! %s" % (file, e))
+        sys.exit(0)
