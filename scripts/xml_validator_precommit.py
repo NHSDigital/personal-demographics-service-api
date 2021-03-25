d8d0ef4aa553be1126731dbbd582f8cc1289277e
paruliansaragi <parulian.saragi1@nhs.net>
2021-03-25 10:13:02 +0000

    Add validate xml to pre-commit

---
+++ scripts/xml_validator_precommit.py
@@ -0,0 +1,16 @@
+import glob2
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
+
+
+def parsefile(file):
+    parser = make_parser()
+    parser.setContentHandler(ContentHandler())
+    parser.parse(file)
+
+
+for file in glob2.glob('./proxies/**/*.xml'):
+    try:
+        parsefile(file)
+    except Exception as e:
+        print("%s is NOT well-formed! %s" % (file, e))
