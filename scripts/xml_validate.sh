7824e03b8b85a682af13a93b9c27e82a63b08f2c
paruliansaragi <parulian.saragi1@nhs.net>
2021-03-25 11:08:11 +0000

    move precommit hook out of makefile.

--- scripts/xml_validate.sh
+++ scripts/xml_validate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # set -euo pipefail
 OUTPUT=$(poetry run python scripts/xml_validator.py)
-if ["$OUTPUT" -eq 0]; then
+
+if [ "$OUTPUT" -eq 0 ]; then
     echo "No XML validation errors!"
     exit 0
 else
