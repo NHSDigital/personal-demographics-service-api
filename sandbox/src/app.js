542c41c973c692db9fa44572094fc626646a2848
Dominic Hui <dominichui112@gmail.com>
2021-05-18 17:16:01 +0100

    testing deployment of hosted target

--- sandbox/src/app.js
+++ sandbox/src/app.js
@@ -10,20 +10,27 @@ const requestValidator = require('./validators/request-validator')
 const CONTENT_TYPE = 'application/fhir+json'
 
 const preHandler = function (request, h) {
-    // check X-Request-ID exists
-    if(!requestValidator.verifyRequestIdHeader(request)){
+    // test change deployment of hosted target
+    if(request.headers["x-request-id"] === "test"){
         throw Boom.preconditionFailed(
             "Invalid request with error - X-Request-ID header must be supplied to access this resource",
             {operationOutcomeCode: "structure", apiErrorCode: "PRECONDITION_FAILED", display: "Required condition was not fulfilled"})
-    }
+        }
 
-    // validate X-Request-ID
-    if(!requestValidator.validateRequestIdHeader(request)){
-        throw Boom.badRequest(
-            "Invalid value - '" + request.headers["x-request-id"] + "' in header 'X-Request-ID'",
-            {operationOutcomeCode: "value", apiErrorCode: "INVALID_VALUE", display: "Provided value is invalid"}
-        )
-    }
+    // check X-Request-ID exists
+    // if(!requestValidator.verifyRequestIdHeader(request)){
+    //     throw Boom.preconditionFailed(
+    //         "Invalid request with error - X-Request-ID header must be supplied to access this resource",
+    //         {operationOutcomeCode: "structure", apiErrorCode: "PRECONDITION_FAILED", display: "Required condition was not fulfilled"})
+    // }
+
+    // // validate X-Request-ID
+    // if(!requestValidator.validateRequestIdHeader(request)){
+    //     throw Boom.badRequest(
+    //         "Invalid value - '" + request.headers["x-request-id"] + "' in header 'X-Request-ID'",
+    //         {operationOutcomeCode: "value", apiErrorCode: "INVALID_VALUE", display: "Provided value is invalid"}
+    //     )
+    // }
 
     return h.continue
 }
