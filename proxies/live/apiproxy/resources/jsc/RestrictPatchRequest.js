a39c4c28a63b97f3172677846aa6cc2351da21fa
Valentinolucas <valentinolucasfantini@gmail.com>
2021-06-22 17:42:10 +0100

    AMB-747 Validate request against nhs-number

---
+++ proxies/live/apiproxy/resources/jsc/RestrictPatchRequest.js
@@ -0,0 +1,9 @@
+var id_token_nhs_number = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.nhs_number');
+var request_pathsuffix = context.getVariable('proxy.pathsuffix');
+var request_path_nhs_number = request_pathsuffix.split("/")[2];
+var method_is_restricted = true
+
+if (request_path_nhs_number == id_token_nhs_number){
+    method_is_restricted = false
+}
+context.setVariable('apigee.method_is_restricted', method_is_restricted);
