b18b137d3bc6d0210cc2ac336b6c8dfbd0a16b63
Valentinolucas <valentinolucasfantini@gmail.com>
2021-06-23 09:37:43 +0100

    AMB-747 Restrict request against nhs-number

--- proxies/live/apiproxy/resources/jsc/RestrictRequest.js
+++ proxies/live/apiproxy/resources/jsc/RestrictRequest.js
@@ -3,6 +3,10 @@ var request_pathsuffix = context.getVariable('proxy.pathsuffix');
 var request_path_nhs_number = request_pathsuffix.split("/")[2];
 var method_is_restricted = true
 
+if (request_pathsuffix.split("/")[1] == "_poll"){
+    method_is_restricted = false
+}
+
 if (request_path_nhs_number == id_token_nhs_number){
     method_is_restricted = false
 }
