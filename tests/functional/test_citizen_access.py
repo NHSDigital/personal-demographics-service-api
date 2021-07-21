a7c071f0bffe46b5364e8e46bb16bba6a367f7d6
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-07-21 14:40:11 +0100

    AMB-747 add update patient citizen access tests

--- tests/functional/test_citizen_access.py
+++ tests/functional/test_citizen_access.py
@@ -1,13 +1,11 @@
 from tests.functional.config_files import config
-from tests.user_restricted.data.pds_scenarios import retrieve, search, update
 import requests
 import uuid
 
 class TestUserRestrictedCitizenAccess:
-    
-    def test_citizen_access_happy_path(self, get_token_nhs_login_token_exchange):
+    def test_citizen_access_retrieve_happy_path(self, get_token_nhs_login_token_exchange):
         token = get_token_nhs_login_token_exchange["access_token"]
-        
+
         headers = {
         "NHSD-SESSION-URID": "123",
         "Authorization": "Bearer " + token,
@@ -17,12 +15,10 @@ class TestUserRestrictedCitizenAccess:
 
         assert r.status_code == 200
 
-    def test_citizen_access_non_matching_nhs_number(self, get_token_nhs_login_token_exchange):
-
-        expected_response = {'resourceType': 'OperationOutcome', 'issue': [{'severity': 'error', 'code': 'token', 'details': {'coding': [{'system': 'https://fhir.nhs.uk/R4/CodeSystem/Spine-ErrorOrWarningCode', 'version': '1', 'code': 'INVALID_VALUE', 'display': 'Cannot retrieve this result with NHS-login Restricted access token'}]}, 'diagnostics': 'Your app has insufficient permissions to perform this search. Please contact support.'}]}
+    def test_citizen_access_retrieve_non_matching_nhs_number(self, get_token_nhs_login_token_exchange):
 
         token = get_token_nhs_login_token_exchange["access_token"]
-        
+
         headers = {
         "NHSD-SESSION-URID": "123",
         "Authorization": "Bearer " + token,
@@ -32,15 +28,14 @@ class TestUserRestrictedCitizenAccess:
         body = r.json()
 
         assert r.status_code == 403
-        assert body == expected_response
-        
+        assert body['issue'][0]['details']['coding'][0]['code'] == 'INVALID_VALUE'
+        assert body['issue'][0]['details']['coding'][0]['display'] == 'Cannot retrieve this result with NHS-login Restricted access token'
 
-    def test_citizen_access_incorrect_path(self, get_token_nhs_login_token_exchange):
 
-        expected_response = {'resourceType': 'OperationOutcome', 'issue': [{'severity': 'error', 'code': 'token', 'details': {'coding': [{'system': 'https://fhir.nhs.uk/R4/CodeSystem/Spine-ErrorOrWarningCode', 'version': '1', 'code': 'INVALID_VALUE', 'display': 'Cannot retrieve this result with NHS-login Restricted access token'}]}, 'diagnostics': 'Your app has insufficient permissions to perform this search. Please contact support.'}]}
+    def test_citizen_access_retrieve_incorrect_path(self, get_token_nhs_login_token_exchange):
 
         token = get_token_nhs_login_token_exchange["access_token"]
-        
+
         headers = {
         "NHSD-SESSION-URID": "123",
         "Authorization": "Bearer " + token,
@@ -50,4 +45,94 @@ class TestUserRestrictedCitizenAccess:
         body = r.json()
 
         assert r.status_code == 403
-        assert body == expected_response
+        assert body['issue'][0]['details']['coding'][0]['code'] == 'INVALID_VALUE'
+        assert body['issue'][0]['details']['coding'][0]['display'] == 'Cannot retrieve this result with NHS-login Restricted access token'
+
+    def test_citizen_access_update_happy_path(self, get_token_nhs_login_token_exchange, create_random_date):
+        token = get_token_nhs_login_token_exchange["access_token"]
+
+        date = create_random_date
+
+        patch_body = {"patches":[{"op":"replace","path":"/birthDate","value":date}]}
+
+        headers = {
+        "NHSD-SESSION-URID": "123",
+        "Authorization": "Bearer " + token,
+        "X-Request-ID": str(uuid.uuid4()),
+    }
+
+        r = requests.get(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient/9912003071", headers=headers)
+        Etag = r.headers['Etag']
+        versionId = r.json()["meta"]["versionId"]
+
+        headers = {
+        "NHSD-SESSION-URID": "123",
+        "Authorization": "Bearer " + token,
+        "X-Request-ID": str(uuid.uuid4()),
+        "If-Match": Etag,
+        "Content-Type": "application/json-patch+json"
+    }
+        r = requests.patch(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient/9912003071", headers=headers, json=patch_body)
+
+        assert r.status_code == 200
+        assert int(r.json()["meta"]["versionId"]) == int(versionId) + 1
+
+    def test_citizen_access_update_non_matching_nhs_number(self, get_token_nhs_login_token_exchange, create_random_date):
+        token = get_token_nhs_login_token_exchange["access_token"]
+
+        date = create_random_date
+
+        patch_body = {"patches":[{"op":"replace","path":"/birthDate","value":date}]}
+
+        headers = {
+        "NHSD-SESSION-URID": "123",
+        "Authorization": "Bearer " + token,
+        "X-Request-ID": str(uuid.uuid4()),
+    }
+
+        r = requests.get(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient/9912003071", headers=headers)
+        Etag = r.headers['Etag']
+
+        headers = {
+        "NHSD-SESSION-URID": "123",
+        "Authorization": "Bearer " + token,
+        "X-Request-ID": str(uuid.uuid4()),
+        "If-Match": Etag,
+        "Content-Type": "application/json-patch+json"
+    }
+        r = requests.patch(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient/123456789", headers=headers, json=patch_body)
+        body = r.json()
+
+        assert r.status_code == 403
+        assert body['issue'][0]['details']['coding'][0]['code'] == 'INVALID_VALUE'
+        assert body['issue'][0]['details']['coding'][0]['display'] == 'Cannot retrieve this result with NHS-login Restricted access token'
+
+    def test_citizen_access_update_incorrect_path(self, get_token_nhs_login_token_exchange, create_random_date):
+        token = get_token_nhs_login_token_exchange["access_token"]
+
+        date = create_random_date
+
+        patch_body = {"patches":[{"op":"replace","path":"/birthDate","value":date}]}
+
+        headers = {
+        "NHSD-SESSION-URID": "123",
+        "Authorization": "Bearer " + token,
+        "X-Request-ID": str(uuid.uuid4()),
+    }
+
+        r = requests.get(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient/9912003071", headers=headers)
+        Etag = r.headers['Etag']
+
+        headers = {
+        "NHSD-SESSION-URID": "123",
+        "Authorization": "Bearer " + token,
+        "X-Request-ID": str(uuid.uuid4()),
+        "If-Match": Etag,
+        "Content-Type": "application/json-patch+json"
+    }
+        r = requests.patch(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient?family=Smith&gender=female&birthdate=eq2010-10-22", headers=headers, json=patch_body)
+        body = r.json()
+
+        assert r.status_code == 403
+        assert body['issue'][0]['details']['coding'][0]['code'] == 'INVALID_VALUE'
+        assert body['issue'][0]['details']['coding'][0]['display'] == 'Cannot retrieve this result with NHS-login Restricted access token'
