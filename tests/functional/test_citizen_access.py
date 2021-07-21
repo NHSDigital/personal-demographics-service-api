3ba09e162ca5034d8c81207534c7d12a93d4bba3
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-07-21 08:42:42 +0100

    AMB-747 update test assertions + add feature toggle

--- tests/functional/test_citizen_access.py
+++ tests/functional/test_citizen_access.py
@@ -1,4 +1,5 @@
 from tests.functional.config_files import config
+from tests.user_restricted.data.pds_scenarios import retrieve, search, update
 import requests
 import uuid
 
@@ -17,6 +18,9 @@ class TestUserRestrictedCitizenAccess:
         assert r.status_code == 200
 
     def test_citizen_access_non_matching_nhs_number(self, get_token_nhs_login_token_exchange):
+
+        expected_response = {'resourceType': 'OperationOutcome', 'issue': [{'severity': 'error', 'code': 'token', 'details': {'coding': [{'system': 'https://fhir.nhs.uk/R4/CodeSystem/Spine-ErrorOrWarningCode', 'version': '1', 'code': 'INVALID_VALUE', 'display': 'Cannot retrieve this result with NHS-login Restricted access token'}]}, 'diagnostics': 'Your app has insufficient permissions to perform this search. Please contact support.'}]}
+
         token = get_token_nhs_login_token_exchange["access_token"]
         
         headers = {
@@ -25,10 +29,15 @@ class TestUserRestrictedCitizenAccess:
         "X-Request-ID": str(uuid.uuid4()),
     }
         r = requests.get(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient/123456789", headers=headers)
+        body = r.json()
 
         assert r.status_code == 403
+        assert body == expected_response
+        
 
     def test_citizen_access_incorrect_path(self, get_token_nhs_login_token_exchange):
+
+        expected_response = {'resourceType': 'OperationOutcome', 'issue': [{'severity': 'error', 'code': 'token', 'details': {'coding': [{'system': 'https://fhir.nhs.uk/R4/CodeSystem/Spine-ErrorOrWarningCode', 'version': '1', 'code': 'INVALID_VALUE', 'display': 'Cannot retrieve this result with NHS-login Restricted access token'}]}, 'diagnostics': 'Your app has insufficient permissions to perform this search. Please contact support.'}]}
 
         token = get_token_nhs_login_token_exchange["access_token"]
         
@@ -38,5 +47,7 @@ class TestUserRestrictedCitizenAccess:
         "X-Request-ID": str(uuid.uuid4()),
     }
         r = requests.get(f"{config.BASE_URL}/{config.PDS_BASE_PATH}/Patient?family=Smith&gender=female&birthdate=eq2010-10-22", headers=headers)
+        body = r.json()
 
         assert r.status_code == 403
+        assert body == expected_response
