b5886505ce1849a42bd4a9688e59ece546f0053a
Dominic Hui <dominichui112@gmail.com>
2021-08-09 16:54:06 +0100

    mock spine polling error - unprocessable entity

--- sandbox/src/routes/patient/polling.js
+++ sandbox/src/routes/patient/polling.js
@@ -16,8 +16,23 @@ module.exports = [
         method: 'GET',
         path: '/_poll/{messageId}',
         handler: (request, h) => {
+
+            // force 422 unprocessable entity to occur if header is provided
+            if("x-force-failure" in request.headers){
+                throw Boom.badData(
+                    "An internal polling message ID was found however the processing of the request failed" +
+                    " in an unexpected way or was cancelled, so the update failed. Please raise these occurrences with our team (via https://digital.nhs.uk/developer/help-and-support)" +
+                    " so we can investigate the issue. When raising, quote the message ID.",
+                    {
+                        operationOutcomeCode: "processing",
+                        apiErrorCode: "POLLING_MESSAGE_FAILURE",
+                        display: "The polling id was found however the processing of the request failed in an unexpected way or was cancelled"
+                    })
+            }
+
             const messageId = request.params.messageId
             const patchedPatient = h.context.messages[messageId];
+
 
             if (messageId === "20200522091633363041_000001") {
                 return fhirHelper.createAcceptedResponse(h, messageId)
