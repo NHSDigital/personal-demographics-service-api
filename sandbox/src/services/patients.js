1bd822ab913757cad94f57424d5eb86bd458288c
paruliansaragi <parulian.saragi1@nhs.net>
2021-09-15 11:19:31 +0100

    add multi name mock

--- sandbox/src/services/patients.js
+++ sandbox/src/services/patients.js
@@ -10,6 +10,7 @@ module.exports = {
     search: {
         exampleSearchPatientSmith: JSON.parse(fs.readFileSync('mocks/PatientSearch.json')),
         exampleSearchPatientSmyth: JSON.parse(fs.readFileSync('mocks/PatientSearch-Jayne-Smyth.json')),
+        exampleSearchPatientSmythMultiName: JSON.parse(fs.readFileSync('mocks/PatientMuliName.json')),
         exampleSearchPatientSmythe: JSON.parse(fs.readFileSync('mocks/Sensitive_PatientSearch.json')),
         exampleSearchPatientMinimal: JSON.parse(fs.readFileSync('mocks/Minimal_PatientSearch.json'))
     }
