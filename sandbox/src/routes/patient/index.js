d8bddc51e623b824d0779077102ea428fd167a01
Malveen <ianmalveen@gmail.com>
2021-09-28 16:42:38 +0100

    Add Minimal_Patient.json back following code review

--- sandbox/src/routes/patient/index.js
+++ sandbox/src/routes/patient/index.js
@@ -1,6 +1,5 @@
 const get = require('./get-patient');
 const patch = require('./patch-patient');
-// const poll = require('./polling');
 const healthcheck = require('./healthcheck');
 
 const routes = [].concat(get, patch, healthcheck);
