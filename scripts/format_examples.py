247b25f459a2e274b865c62340bcebdbd92a9e2e
paruliansaragi <parulian.saragi1@nhs.net>
2021-12-17 09:49:18 +0000

    add managing org to schema

--- scripts/format_examples.py
+++ scripts/format_examples.py
@@ -205,7 +205,8 @@ def sensitive_patient(resource):
         "address",
         "telecom",
         "contact",
-        "generalPractitioner"
+        "generalPractitioner",
+        "managingOrganization"
     ]
     new_resource = _process_blacklist(resource, blacklist)
     new_resource["meta"]["security"][0]["code"] = "R"
