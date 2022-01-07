b5d14cde3ee928139bc2af01532cdbda317f116e
Alex Hawdon <alex.hawdon@airelogic.com>
2022-01-07 00:23:47 +0000

    reinstate

--- tests/functional/config_files/config.py
+++ tests/functional/config_files/config.py
@@ -53,6 +53,7 @@ JWKS_RESOURCE_URL_ASID_REQUIRED_APP = ('https://raw.githubusercontent.com/NHSDig
 
 # JWT keys
 ID_TOKEN_NHS_LOGIN_PRIVATE_KEY = ENV['nhs_login_private_key']
+JWT_PRIVATE_KEY_ABSOLUTE_PATH = ENV['jwt_private_key']
 
 AUTH_TOKEN_EXPIRY_MS = (
     ENV['auth_token_expiry_ms'] if ENVIRONMENT == "internal-dev" else ENV['auth_token_expiry_ms_int']
