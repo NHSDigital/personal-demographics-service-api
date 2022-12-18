ec57fa0a46cb1b20c1c9c59ef3428183ed17a918
Kris Szlapa <kris.szlapa1@nhs.net>
2022-12-18 01:02:18 +0000

    SPINEDEM-649 Return an object with verifyAddressIdNotPresentWhenRequired

--- sandbox/src/validators/request-validator.js
+++ sandbox/src/validators/request-validator.js
@@ -56,15 +56,11 @@ module.exports = {
         }
         
         if (idRequired && !idPresent) {
-            return [`Invalid update with error - no id or url found for path with root /${pathValue}/0`,
-            {operationOutcomeCode: "structure", apiErrorCode: "INVALID_UPDATE", display: "Update is invalid"}]
+            return {0: pathValue} 
         } else if (idRequired && idPresent && addressPresent && validId != true) {
-            return [`Invalid update with error - no '${pathValue}' resources with object id ${wrongId}`,
-            {operationOutcomeCode: "structure", apiErrorCode: "INVALID_UPDATE", display: "Update is invalid"}]
+            return {1: pathValue, 2: wrongId}
         } else if (idRequired && idPresent && !addressPresent) {
-            return ["Invalid update with error - Invalid patch - index '0' is out of bounds",
-            {operationOutcomeCode: "structure", apiErrorCode: "INVALID_UPDATE", display: "Update is invalid"}]
-        } else return
-    }
+            return {3: ""}
+        }}
     }
 }
