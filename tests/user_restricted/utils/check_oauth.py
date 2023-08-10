a0f8b3a57a3b8ae4d072a058fe9ba98026e1ee9c
Richard Coates <richard.coates5@nhs.net>
2023-08-10 15:04:50 +0000

    SPINEDEM-1289 Raise error to work out what the redirect url is

--- tests/user_restricted/utils/check_oauth.py
+++ tests/user_restricted/utils/check_oauth.py
@@ -11,6 +11,7 @@ class CheckOauth:
 
     @staticmethod
     def get_token_response():
+        raise RuntimeError(f"redirect uri!!!! {config.REDIRECT_URI}")
         oauth = OauthHelper(config.CLIENT_ID, config.CLIENT_SECRET, config.REDIRECT_URI)
         token_resp = oauth.get_authenticated_with_mock_auth(user=config.IDENTITY_SERVICE_MOCK_USER_ID)
         return token_resp
