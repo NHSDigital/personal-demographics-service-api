3e2958e41b849cc72e885fc865908a80ff1082e6
david-hamill <david-hamill1@nhs.net>
2023-08-31 19:38:37 +0100

    Correct flake8 errors.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,11 @@ import pytest
 pytest_plugins = ["pytest_nhsd_apim.apigee_edge",
                   "pytest_nhsd_apim.secrets"]
 
+
 @pytest.fixture()
 def status_endpoint_api_key():
     return os.environ.get('STATUS_ENDPOINT_API_KEY', 'not-set')
+
 
 @pytest.fixture()
 def status_endpoint_header(status_endpoint_api_key):
