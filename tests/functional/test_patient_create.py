c2d235e8795670009ef8cdf74188e9c43945a2c2
richardseacome1-nhs <richard.seacome1@nhs.net>
2024-03-06 18:12:16 +0000

    SPINEDEM-2186: reformat for pylint error resolution

--- tests/functional/test_patient_create.py
+++ tests/functional/test_patient_create.py
@@ -113,13 +113,13 @@ async def _create_all_patients(headers, url, body, loop, num_patients):
 def post_patient_multiple_times(healthcare_worker_auth_headers: dict, pds_url: str) -> list:
     patients_to_create = 27
     url = f'{pds_url}/Patient'
-    body = json.dumps({'type': 'object', 'properties': {'nhsNumberAllocation': {'type': 'string', 
-                        'description': 'FHIR resource type.', 'enum': ['Done'], 'example': 'Done'}}, 
-                        'required': ['nhsNumberAllocation']})
+    body = json.dumps({'type': 'object', 'properties': {'nhsNumberAllocation': {'type': 'string',
+                        'description': 'FHIR resource type.', 'enum': ['Done'], 'example': 'Done'}},
+                       'required': ['nhsNumberAllocation']})
 
     loop = asyncio.new_event_loop()
     results = loop.run_until_complete(
-         _create_all_patients(healthcare_worker_auth_headers, url, body, loop, patients_to_create)
+        _create_all_patients(healthcare_worker_auth_headers, url, body, loop, patients_to_create)
     )
     request_times = [x['request_time'] for x in results]
     request_times.sort()
