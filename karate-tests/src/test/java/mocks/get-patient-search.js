e6e1d2e1e2db14f317ac29b1eff4a328ee563238
Peter Loggie <peter.loggie1@nhs.net>
2024-03-27 20:21:46 +0100

    removes comments and cleans up logic a bit

--- karate-tests/src/test/java/mocks/get-patient-search.js
+++ karate-tests/src/test/java/mocks/get-patient-search.js
@@ -60,22 +60,9 @@ function invalidUpdateError(request, diagnostics) {
     Specific query param validation to support main handler
 */
 function otherJaneSmithParamsAreValid(request) {    
-    if (request.param('phone') && request.param('phone') != "01632960587") {
-        context.log("phone: " + request.param('phone'))
-        return false
-    }
-    if (request.param('email') && request.param('email') != "jane.smith@example.com") {
-        context.log("email: " + request.param('email'))
-        return false
-    }
+    if (request.param('phone') && request.param('phone') != "01632960587") return false
+    if (request.param('email') && request.param('email') != "jane.smith@example.com") return false
     return true
-    // const exactMatch = request.param('_exact-match')
-    // const history = request.param('history')
-    // const maxResults = request.param('_max-results')
-    // const given = request.param('given')
-    // const deathDate = request.param('death-date')
-    // const postalCode = request.param('address-postcode');
-    // const gp = request.param('general-practitioner')
 }
 
 
@@ -89,55 +76,49 @@ if (request.pathMatches('/Patient') && request.get) {
 
     const paramsCount = Object.keys(request.params).length
     if (paramsCount == 0) {
-        returnMissingValueError(NOT_ENOUGH_SEARCH_PARAMS, request)
+        valid = returnMissingValueError(NOT_ENOUGH_SEARCH_PARAMS, request)
     }
 
     const family = request.param('family')
     const given = request.params['given']
-    context.log("given: " + given)
-    context.log(given == ["John Paul","James"])
     const gender = request.param('gender')
     const birthDate = request.params['birthdate']
     const fuzzyMatch = request.paramBool('_fuzzy-match')
     const phone = request.param('phone')
     const email = request.param('email')
 
-    // context.log(request.params)
-    if (!valid) {
-        response.body = "not valid"
-        response.status = 401
-    }
-    else if (fuzzyMatch) {
-        if (!phone && !email) {
-            response.body = FUZZY_SEARCH_PATIENT_17
-        } else if (phone == "01632960587" && !email) {
-            response.body = janeSmithSearchsetWithScore(0.9124)
-        } else if (email == "jane.smith@example.com" && !phone) {
-            response.body = janeSmithSearchsetWithScore(0.9124)
-        } else if (phone == "01632960587" && email == "jane.smith@example.com") {
-            response.body = janeSmithSearchsetWithScore(0.9542)
+    if (valid) {
+        if (fuzzyMatch) {
+            if (!phone && !email) {
+                response.body = FUZZY_SEARCH_PATIENT_17
+            } else if (phone == "01632960587" && !email) {
+                response.body = janeSmithSearchsetWithScore(0.9124)
+            } else if (email == "jane.smith@example.com" && !phone) {
+                response.body = janeSmithSearchsetWithScore(0.9124)
+            } else if (phone == "01632960587" && email == "jane.smith@example.com") {
+                response.body = janeSmithSearchsetWithScore(0.9542)
+            }
+        }
+        else if (["Sm*", "sm*"].includes(family)) {
+            if (!phone && !email) {
+                response.body = WILDCARD_SEARCH
+            } else if (phone == "01632960587" && !email) {
+                response.body = janeSmithSearchsetWithScore(1)
+            } else if (email == "jane.smith@example.com" && !phone) {
+                response.body = janeSmithSearchsetWithScore(1)
+            }
+        }
+        else if (["Smythe", "smythe"].includes(family)) {
+            response.body = RESTRICTED_PATIENT_SEARCH
+        }
+        else if (["Smith", "smith"].includes(family) && ["Female", "female"].includes(gender) && (birthDate == "eq2010-10-22" || birthDate == "ge2010-10-21,le2010-10-23") && (otherJaneSmithParamsAreValid(request))) {
+            response.body = SIMPLE_SEARCH
+        }
+        else if (["Smith", "smith"].includes(family) && ["Male", "male"].includes(gender) && given.length == 2) {
+            response.body = COMPOUND_NAME_SEARCH
+        }    
+        else {
+            response.body = EMPTY_SEARCHSET
         }
     }
-    else if (["Sm*", "sm*"].includes(family)) {
-        if (!phone && !email) {
-            response.body = WILDCARD_SEARCH
-        } else if (phone == "01632960587" && !email) {
-            response.body = janeSmithSearchsetWithScore(1)
-        } else if (email == "jane.smith@example.com" && !phone) {
-            response.body = janeSmithSearchsetWithScore(1)
-        }
-    }
-    else if (["Smythe", "smythe"].includes(family)) {
-        response.body = RESTRICTED_PATIENT_SEARCH
-    }
-    else if (["Smith", "smith"].includes(family) && ["Female", "female"].includes(gender) && (birthDate == "eq2010-10-22" || birthDate == "ge2010-10-21,le2010-10-23") && (otherJaneSmithParamsAreValid(request))) {
-        response.body = SIMPLE_SEARCH
-    }
-    else if (["Smith", "smith"].includes(family) && ["Male", "male"].includes(gender) && given.length == 2) {
-        response.body = COMPOUND_NAME_SEARCH
-    }    
-    else {
-        response.body = EMPTY_SEARCHSET
-    }
-
 }
