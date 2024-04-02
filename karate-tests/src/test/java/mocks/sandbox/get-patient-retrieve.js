d26f2cac84441fc271f7fd386b1e9a0eb244a5b1
Peter Loggie <peter.loggie1@nhs.net>
2024-04-02 12:55:57 +0200

    adds eslint for karate mocks, and lints the karate mock js files

--- karate-tests/src/test/java/mocks/sandbox/get-patient-retrieve.js
+++ karate-tests/src/test/java/mocks/sandbox/get-patient-retrieve.js
@@ -1,14 +1,20 @@
 /*
     Handler for get patient by NHS Number
 */
-if (request.pathMatches('/Patient/{nhsNumber}') && request.get) {  
-    let valid = validateHeaders(request) && validateNHSNumber(request) && validatePatientExists(request)
-    if (valid) {
-        patient = session.patients[request.pathParams.nhsNumber]
-        let responseHeaders = basicResponseHeaders(request)
-        responseHeaders['etag'] = `W/"${patient.meta.versionId}"`
-        response.body = patient
-        response.headers = responseHeaders
-        response.status = 200
-    }
+
+/* Karate objects */
+/* global request, response, session */
+
+/* Functions defined in supporting-functions.js */
+/* global validateHeaders, validateNHSNumber, validatePatientExists, basicResponseHeaders */
+
+if (request.pathMatches('/Patient/{nhsNumber}') && request.get) {
+  if (validateHeaders(request) && validateNHSNumber(request) && validatePatientExists(request)) {
+    const patient = session.patients[request.pathParams.nhsNumber]
+    const responseHeaders = basicResponseHeaders(request)
+    responseHeaders.etag = `W/"${patient.meta.versionId}"`
+    response.body = patient
+    response.headers = responseHeaders
+    response.status = 200
+  }
 }
