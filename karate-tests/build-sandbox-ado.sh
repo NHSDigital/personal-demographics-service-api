e0391aebf116a742c383fc1dbdf11e70911259c9
Peter Loggie <peter.loggie1@nhs.net>
2024-04-12 10:30:39 +0200

    try building karate jar in ado

--- karate-tests/build-sandbox-ado.sh
+++ karate-tests/build-sandbox-ado.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -x -e
-wget "$KARATE_JARFILE_URL" > /tmp/karate.jar
+git clone https://github.com/karatelabs/karate.git karate
+cd karate
+git checkout c0597e5b7fcd61be47f916854e8473da11b37956
+mvn clean install -P pre-release -pl -karate-robot,-karate-playwright,-karate-e2e-tests
+cd karate-core
+mvn package -P fatjar
+cp target/karate-core-1.6.0-SNAPSHOT.jar /tmp/karate.jar
 docker build -t nhs/pds-sandbox:latest .
 docker run -d --name karate-sandbox nhs/pds-sandbox
