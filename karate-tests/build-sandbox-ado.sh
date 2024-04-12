c1c02fc747e7191e2b4a98103c588742171ef2dd
Peter Loggie <peter.loggie1@nhs.net>
2024-04-12 13:12:54 +0200

    remove container if exists

--- karate-tests/build-sandbox-ado.sh
+++ karate-tests/build-sandbox-ado.sh
@@ -2,10 +2,13 @@
 set -x -e
 docker build -t nhs/pds-sandbox:latest .
 
-# Check if the container is running
-if docker container inspect -f '{{.State.Running}}' karate-sandbox >/dev/null 2>&1; then
-    # Stop the container
-    docker container stop karate-sandbox >/dev/null
+# Check if the container exists
+if docker container inspect karate-sandbox >/dev/null 2>&1; then
+    # Check if the container is running
+    if docker container inspect -f '{{.State.Running}}' karate-sandbox >/dev/null 2>&1; then
+        # Stop the container
+        docker container stop karate-sandbox >/dev/null
+    fi
     # Remove the container
     docker container rm karate-sandbox >/dev/null
 fi
