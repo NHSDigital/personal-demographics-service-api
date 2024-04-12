4ae447c7f349e789214c1c8b8eea7e35be5fca97
Peter Loggie <peter.loggie1@nhs.net>
2024-04-12 15:27:25 +0200

    testing out different ways of running the sandbox tests

--- tests/sandbox/configuration/config.py
+++ tests/sandbox/configuration/config.py
@@ -3,11 +3,11 @@ from .environment import ENV
 
 # Sandbox Details
 ENVIRONMENT = ENV["environment"]
-CONTAINER_IP = ENV["container_ip"]
 
 if ENVIRONMENT.lower() == "local":
     SANDBOX_BASE_URL = "http://0.0.0.0:9000"
 elif ENVIRONMENT.lower() == "docker":
+    CONTAINER_IP = ENV["container_ip"]
     SANDBOX_BASE_URL = f"http://{CONTAINER_IP}:9090"
 else:
     SERVICE_BASE_PATH = ENV["service_base_path"]
