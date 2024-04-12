7350fc0b82bbba4b78623b450f4f51c8192a941d
Peter Loggie <peter.loggie1@nhs.net>
2024-04-12 13:01:43 +0200

    how was the container running in ado?

--- karate-tests/build-sandbox-ado.sh
+++ karate-tests/build-sandbox-ado.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -x -e
 docker build -t nhs/pds-sandbox:latest .
-docker run -d --name karate-sandbox nhs/pds-sandbox
+
+# Check if the container is running
+if docker container inspect -f '{{.State.Running}}' karate-sandbox >/dev/null 2>&1; then
+    # Stop the container
+    docker container stop karate-sandbox >/dev/null
+    # Remove the container
+    docker container rm karate-sandbox >/dev/null
+fi
+
+# Run the container
+docker run -d --name karate-sandbox -p 9090:9090 nhs/pds-sandbox:latest
