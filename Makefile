f556dd3786d4f7bef46d82ab218073fa20a59e39
Peter Loggie <peter.loggie1@nhs.net>
2024-04-12 10:57:58 +0200

    try building jarfile in github instead of ado

--- Makefile
+++ Makefile
@@ -83,6 +83,7 @@ release: clean publish build-proxy
 	cp -R build/. dist/
 	cp -R terraform dist
 	cp -R tests dist
+	cp /tmp/karate/karate-core-1.6.0-SNAPSHOT.jar karate-tests/karate.jar
 	cp -R karate-tests dist
 
 	cp ecs-proxies-deploy.yml dist/ecs-deploy-sandbox.yml
--- karate-tests/build-sandbox-ado.sh
+++ karate-tests/build-sandbox-ado.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -x -e
-git clone https://github.com/karatelabs/karate.git karate
-cd karate
-git checkout c0597e5b7fcd61be47f916854e8473da11b37956
-mvn clean install -P pre-release -pl -karate-robot,-karate-playwright,-karate-e2e-tests
-cd karate-core
-mvn package -P fatjar
-cp target/karate-core-1.6.0-SNAPSHOT.jar /tmp/karate.jar
 docker build -t nhs/pds-sandbox:latest .
 docker run -d --name karate-sandbox nhs/pds-sandbox
