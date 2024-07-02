25ca932c14164e69f0628bcd54a50bd8fd3f2cc8
Peter Loggie <peter.loggie1@nhs.net>
2024-07-02 15:16:52 +0100

    removes more pytest stuff we don't need

--- tests/functional/features/patient_access_authentication.feature
+++ tests/functional/features/patient_access_authentication.feature
@@ -1,15 +1,5 @@
 Feature: Patient Access - authentication
-	The P9 patient can authenticate and retrieve their details
 	Our P5 and P0 patients cannot authenticate
-
-	Scenario: Patient can retrieve self
-		Given I am a P9 user
-		And scope added to product
-		
-		When I retrieve my details
-
-		Then I get a 200 HTTP response code
-
 	Scenario: Patient with P5 authorisation level cannot authenticate
 		Given I am a P5 user with the NHS number linked to an account
 		
--- tests/functional/test_patient_access.py
+++ tests/functional/test_patient_access.py
@@ -1,38 +1,9 @@
-import pytest
 import pytest_bdd
 
 from functools import partial
 
-from pytest_bdd import given
-
-from tests.functional.data import patients
-from tests.functional.data.patients import Patient
-from tests.functional.data.updates import Update
-
-
-@pytest.fixture()
-def patient() -> Patient:
-    return patients.SELF
-
-
-@pytest.fixture()
-def update(patient: Patient) -> Update:
-    return patient.update
-
-
-@given("I have another patient's NHS number", target_fixture="patient")
-def patient_other() -> Patient:
-    patient = patients.DEFAULT
-    patient.update = Update(nhs_number=patient.nhs_number)
-    return patient
-
 
 auth_scenario = partial(pytest_bdd.scenario, './features/patient_access_authentication.feature')
-
-
-@auth_scenario('Patient can retrieve self')
-def test_retrieve_self():
-    pass
 
 
 @auth_scenario('Patient with P5 authorisation level cannot authenticate')
