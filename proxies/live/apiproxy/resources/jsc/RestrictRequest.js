fd4cf2c0836cf8d5e393b08bc78ac81de9e96da0
Peter Gallagher <Peter.Gallagher6@nhs.net>
2024-09-10 08:57:22 +0100

    More debug lines

--- proxies/live/apiproxy/resources/jsc/RestrictRequest.js
+++ proxies/live/apiproxy/resources/jsc/RestrictRequest.js
@@ -1,5 +1,6 @@
 var id_token_nhs_number = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.nhs_number');
-context.setVariable('claim_debug', context.getVariable(jwt.DecodeJWT.DecodeIdToken.claim));
+context.setVariable('claim_debug', context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim'));
+context.setVariable('vot_debug', context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.vot'));
 var request_pathsuffix = context.getVariable('proxy.pathsuffix');
 var request_path_nhs_number = request_pathsuffix.split("/")[2];
 var method_is_restricted = true
