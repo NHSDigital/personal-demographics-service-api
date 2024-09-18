5efc13b10cc71b6b061ef972ac2a86bdd9bf1c0e
richardseacome1-nhs <richard.seacome1@nhs.net>
2024-09-18 13:08:23 +0100

    SPINEDEM-3794: code to check for vot against approved list

--- proxies/live/apiproxy/resources/jsc/RestrictRequest.js
+++ proxies/live/apiproxy/resources/jsc/RestrictRequest.js
@@ -3,6 +3,12 @@ var request_pathsuffix = context.getVariable('proxy.pathsuffix');
 var request_path_nhs_number = request_pathsuffix.split("/")[2];
 var method_is_restricted = true
 var sync_wrapped = context.getVariable('request.header.x-sync-wrapped');
+var vot = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.vot');
+const allowed_vots = ["P9.Cp.Cd","P9.Cm","P9.Cp.Ck"];
+
+if (!allowed_vots.includes(vot)) {
+    method_is_restricted = false
+}
 
 if (request_pathsuffix.split("/")[1] == "_poll" && sync_wrapped == "true"){
     method_is_restricted = false
