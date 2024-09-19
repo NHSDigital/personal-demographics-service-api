0471fc62a911f330142aeca2c5d4771b1b0a4b91
richardseacome1-nhs <richard.seacome1@nhs.net>
2024-09-19 13:26:40 +0100

    SPINEDEM-3794: Some further test adjustments

--- proxies/live/apiproxy/resources/jsc/RestrictRequest.js
+++ proxies/live/apiproxy/resources/jsc/RestrictRequest.js
@@ -6,10 +6,6 @@ var sync_wrapped = context.getVariable('request.header.x-sync-wrapped');
 var vot = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.vot');
 var allowed_vots = ["P9.Cp.Cd","P9.Cm","P9.Cp.Ck"];
 
-// if (allowed_vots.indexOf(vot) > -1) {
-//     method_is_restricted = false
-// }
-
 if (request_pathsuffix.split("/")[1] == "_poll" && sync_wrapped == "true"){
     method_is_restricted = false
 }
