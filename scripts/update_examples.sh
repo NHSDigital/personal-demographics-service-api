30587ee073204b8d8fa0fd26cab7bde194dfcf21
Peter Gallagher <Peter.Gallagher6@nhs.net>
2025-02-07 17:04:08 +0000

    Doc examples for POST coverage

--- scripts/update_examples.sh
+++ scripts/update_examples.sh
@@ -8,8 +8,11 @@ jq -rM . <build/examples/resources/Sensitive_Patient.json >specification/compone
 jq -rM . <build/examples/resources/Sensitive_PatientSearch.json >specification/components/examples/Sensitive_PatientSearch.json
 jq -rM . <build/examples/resources/Minimal_Patient.json >specification/components/examples/Minimal_Patient.json
 jq -rM . <build/examples/resources/Minimal_PatientSearch.json >specification/components/examples/Minimal_PatientSearch.json
+jq -rM . <build/examples/resources/PatientSearch-CompoundName.json >specification/components/examples/PatientCompoundName.json
+
 jq -rM . <build/examples/resources/RelatedPersonBundle.json >specification/components/examples/RelatedPersonBundle.json
 jq -rM . <build/examples/resources/Referenced_RelatedPersonBundle.json >specification/components/examples/Referenced_RelatedPersonBundle.json
 jq -rM . <build/examples/resources/Personal_Details_RelatedPersonBundle.json >specification/components/examples/Personal_Details_RelatedPersonBundle.json
-jq -rM . <build/examples/resources/PatientSearch-CompoundName.json >specification/components/examples/PatientCompoundName.json
+
 jq -rM . <build/examples/resources/CoverageSearch.json >specification/components/examples/CoverageSearch.json
+jq -rM . <build/examples/resources/Coverage.json >specification/components/examples/Coverage.json
