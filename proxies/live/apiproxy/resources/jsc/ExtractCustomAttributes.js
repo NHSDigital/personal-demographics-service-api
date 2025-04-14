fb4960bf35c9dde6af87539f1c01ef1ae1e5d569
Fabian McGibbon <fabian.mcgibbon1@nhs.net>
2025-04-14 11:35:45 +0000

    SPINEDEM-4939 Fix unit tests for ExtractCustomAttributes.js

--- proxies/live/apiproxy/resources/jsc/ExtractCustomAttributes.js
+++ proxies/live/apiproxy/resources/jsc/ExtractCustomAttributes.js
@@ -1,12 +1,8 @@
 function extractCustomAttributes(json) {
     if (json) {
-        try {
-            var parsed = JSON.parse(json);
-            if (parsed.pds && parsed.pds["custom-attributes"]) {
-                context.setVariable("extractedCustomAttributes", JSON.stringify(parsed.pds["custom-attributes"]))
-            }
-        } catch (e) {
-            context.setVariable("extractedCustomAttributesException", e.message)
+        var parsed = JSON.parse(json);
+        if (parsed.pds && parsed.pds["custom-attributes"]) {
+            context.setVariable("extractedCustomAttributes", JSON.stringify(parsed.pds["custom-attributes"]))
         }
     }
 }
