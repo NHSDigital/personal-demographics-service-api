7a026743419c2bc662eb3919d255853600c0ff75
Daniel Yip <daniel.yip4@nhs.net>
2025-06-20 16:33:40 +0100

    SPINEDEM-5192 Action Sonar warnings

--- karate-tests/src/test/java/auth-jwt/app-restricted-headers.js
+++ karate-tests/src/test/java/auth-jwt/app-restricted-headers.js
@@ -1,14 +1,12 @@
 function fn() {
-  var accessToken = karate.get('accessToken')
-  var correlation_id = '' + java.util.UUID.randomUUID();
-  var request_id = '' + java.util.UUID.randomUUID();
+  const accessToken = karate.get('accessToken')
+  const correlation_id = '' + java.util.UUID.randomUUID();
+  const request_id = '' + java.util.UUID.randomUUID();
 
-  var headers = {
+  return {
     "nhsd-session-id": "123",
     "x-request-id": request_id,
     "x-correlation-id": correlation_id,
     "authorization": "Bearer " + accessToken
-  }
-
-  return headers;
+  };
 }
