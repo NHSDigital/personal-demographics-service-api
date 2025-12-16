69e7419d66fba5f1d3aa0219531c8f1fcf7c1e9f
Kieron Harper <kieron.harper1@nhs.net>
2025-12-16 09:47:39 +0000

    SPINEDEM-5718 resolving proxy deploy issue with ExtractCustomAttributes.js

--- proxies/live/apiproxy/resources/jsc/ExtractCustomAttributes.js
+++ proxies/live/apiproxy/resources/jsc/ExtractCustomAttributes.js
@@ -1,7 +1,9 @@
 function extractCustomAttributes(json) {
     if (json) {
         const parsed = JSON.parse(json);
-        if (parsed?.pds?.["custom-attributes"]) {
+        // Cannot use optional chaining (?.) as Apigee JavaScript runtime doesn't support ES2020 syntax
+        // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+        if (parsed && parsed.pds && parsed.pds["custom-attributes"]) {
             context.setVariable("extractedCustomAttributes", JSON.stringify(parsed.pds["custom-attributes"]))
         }
     }
