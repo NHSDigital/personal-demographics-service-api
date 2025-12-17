25a99e4523d15c83a97fc10bddc592bce9b321e1
Kieron Harper <kieron.harper1@nhs.net>
2025-12-17 16:44:16 +0000

    SPINEDEM-5718 reverting RestrictRequest.js to original form, causes failures with karate-tests

--- proxies/live/apiproxy/resources/jsc/RestrictRequest.js
+++ proxies/live/apiproxy/resources/jsc/RestrictRequest.js
@@ -1,24 +1,27 @@
+/* eslint-disable no-var, vars-on-top */
+// Apigee JavaScript runtime doesn't support ES6 features (let/const)
+// Must use 'var' instead of 'const'/'let' for variable declarations
 function is_request_restricted() {
-    const splitPathsuffix = context.getVariable('proxy.pathsuffix').split("/");
-    const fullUrl = context.getVariable('proxy.url')
+    var splitPathsuffix = context.getVariable('proxy.pathsuffix').split("/");
+    var fullUrl = context.getVariable('proxy.url')
 
     // Ignore polling
-    const sync_wrapped = context.getVariable('request.header.x-sync-wrapped');
+    var sync_wrapped = context.getVariable('request.header.x-sync-wrapped');
     if (splitPathsuffix[1] == "_poll" && sync_wrapped == "true"){
         return false
     }
 
     // Ensure correct vector of trust
-    const allowed_vots = ["P9.Cp.Cd","P9.Cm","P9.Cp.Ck"];
-    const vot_on_request = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.vot');
+    var allowed_vots = ["P9.Cp.Cd","P9.Cm","P9.Cp.Ck"];
+    var vot_on_request = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.vot');
     if (!allowed_vots.includes(vot_on_request)) {
         return true
     }
 
-    const id_token_nhs_number = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.nhs_number');
-    const coverageRegex = new RegExp(/Coverage\?subscriber%3Aidentifier=\d{10}$/)
+    var id_token_nhs_number = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.nhs_number');
+    var coverageRegex = new RegExp(/Coverage\?subscriber%3Aidentifier=\d{10}$/)
     if (splitPathsuffix[1] == "Coverage"){
-        const httpVerb = context.getVariable('request.verb');
+        var httpVerb = context.getVariable('request.verb');
         if (httpVerb === "POST") {
             return false
         }
@@ -32,7 +35,7 @@ function is_request_restricted() {
         return false
     }
 
-    const request_path_nhs_number = splitPathsuffix[2];
+    var request_path_nhs_number = splitPathsuffix[2];
     if ((request_path_nhs_number != id_token_nhs_number)) {
         return true
     }
